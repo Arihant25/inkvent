22226d89ae6abbd83361dd5f27dfd9645f2e4880
Arihant Tripathy <arihanttr@live.com>
2023-12-29 16:28:33 +0530

    Migrate to static

--- app.py
+++ app.py
@@ -1,46 +1,57 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, send_from_directory
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
 import os
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-# Connect to DB
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DATABASE_URL", "sqlite:///" + os.path.join(basedir, "articles.sqlite")
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+import os
+
+REPO_NAME = "inkvent"  # Used for FREEZER_BASE_URL
+DEBUG = True
+
+# Assumes the app is located in the same directory where this file resides
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-# Create DB
-class Article(db.Model):
-    """Database table containing all articles."""
-
-    __tablename__ = "articles"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(10000))
-    subtitle = db.Column(db.String(10000))
-    date = db.Column(db.String(10000))
-    body = db.Column(db.Text)
-    author = db.Column(db.String(10000))
-    img_url = db.Column(db.String(10000))
-    song_url = db.Column(db.String(10000))
+def parent_dir(path):
+    """Return the parent of a directory."""
+    return os.path.abspath(os.path.join(path, os.pardir))
 
 
-@app.route("/")
+PROJECT_ROOT = parent_dir(APP_DIR)
+# In order to deploy to Github pages, you must build the static files to the project root
+FREEZER_DESTINATION = PROJECT_ROOT
+# Since this is a repo page we need to set the BASE_URL to the correct url as per GH Pages' standards
+FREEZER_BASE_URL = "http://localhost/{0}".format(REPO_NAME)
+FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files will be deleted when you run the freezer
+FLATPAGES_MARKDOWN_EXTENSIONS = ["codehilite"]
+FLATPAGES_ROOT = os.path.join(APP_DIR, "articles")
+FLATPAGES_EXTENSION = ".md"
+
+pages = FlatPages(app)
+freezer = Freezer(app)
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/index/")
 def get_all_articles():
-    articles = db.session.query(Article).all()
+    articles = [article for article in pages]
+    # Sort pages by date
+    # sorted_articles = sorted(articles, reverse=True, key=lambda page: page.meta["date"])
     return render_template("index.html", all_articles=articles)
 
 
-@app.route("/article/<int:index>")
-def show_article(index):
-    requested_article = db.session.query(Article).get(index)
-    return render_template("article.html", article=requested_article)
+# @app.route("/article/<int:index>/")
+# def show_article(index):
+#     requested_article = pages.get_or_404(str(index))
+#     return render_template("article.html", article=requested_article)
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+if __name__ == "__main__":
+    freezer.freeze()
